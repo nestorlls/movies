1da4f656c687cceb5607d8d72c76284436749dc1
Paul Llanque <llanquepol@gmail.com>
2023-03-23 10:05:56 -0500

    update extention input and styles

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,3 +1,17 @@
+import styled from '@emotion/styled';
+
+const StyledImput = styled.input`
+  border-radius: 6px;
+  box-shadow: 0px 1px 2px rgba(0, 0, 0, 0.05);
+  padding: 8px 12px;
+  border: 1px solid #d3d3d3;
+  background: #ffffff;
+  width: 100%;
+  &:focus {
+    outline: none;
+    border: 1px solid #d3d3d3;
+  }
+`;
 export const Input = ({
   id,
   name,
@@ -10,7 +24,7 @@ export const Input = ({
   return (
     <div>
       {label && <label htmlFor={id || name}>{label}:</label>}
-      <input
+      <StyledImput
         type={type}
         name={name}
         id={id || name}
