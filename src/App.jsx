adcd7240479fbf5116e5ca66f7337fee79c8d066
Paul Llanque <llanquepol@gmail.com>
2023-03-23 10:55:59 -0500

    add seach test

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
+import { useState } from 'react';
+import SearchForm from './components/pages/SearchForm';
+import { getMovies } from './components/services/MovieService';
+
 function App() {
+  const [movies, setMovies] = useState([]);
+
+  function searchMovies(query) {
+    getMovies(query).then((data) => setMovies(data.results));
+  }
   return (
     <div>
       <h1 style={{ textAlign: 'center' }}>🎥 Movies workshop 🎥</h1>
+      <SearchForm onSubmit={searchMovies} />
+      <ul>
+        {movies.map((movie) => (
+          <li key={movie.id}>{movie.original_title}</li>
+        ))}
+      </ul>
     </div>
   );
 }
