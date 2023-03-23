532c7bd97289bb758ff0d5edb62bf9f8dcdb65c7
Paul Llanque <llanquepol@gmail.com>
2023-03-23 14:47:23 -0500

    add Pill component for genres

---
+++ src/components/Pill.jsx
@@ -0,0 +1,17 @@
+import styled from '@emotion/styled';
+
+const Pill = styled.div`
+  background: #4b5563;
+  border-radius: 24px;
+  padding: 4px 8px;
+  display: flex;
+  width: fit-content;
+  justify-content: center;
+  align-items: center;
+  font-weight: 400;
+  font-size: 12px;
+  line-height: 12px;
+  color: #ffffff;
+`;
+
+export default Pill;
