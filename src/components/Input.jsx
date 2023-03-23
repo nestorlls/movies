d2fe2e786e99774ae519e83063d36634d76605ea
Paul Llanque <llanquepol@gmail.com>
2023-03-23 09:58:53 -0500

    add input component

---
+++ src/components/Input.jsx
@@ -0,0 +1,23 @@
+export const Input = ({
+  id,
+  name,
+  type = 'text',
+  value,
+  onChange,
+  placeholder,
+  label,
+}) => {
+  return (
+    <div>
+      {label && <label htmlFor={id || name}>{label}:</label>}
+      <input
+        type={type}
+        name={name}
+        id={id || name}
+        value={value}
+        onChange={onChange}
+        placeholder={placeholder}
+      />
+    </div>
+  );
+};
